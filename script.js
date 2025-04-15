319c40d6c4fe0ef9bd077b0c18649f51e60c8601
Gaurabpun <118090173+Gaurabpun@users.noreply.github.com>
2025-04-15 02:13:07 -0400

    Updated

--- script.js
+++ script.js
@@ -92,11 +92,7 @@ document.addEventListener("scroll", function() {
 
 function toggleDetails(id) {
   const element = document.getElementById(id);
-  if (element.style.display === "none" || element.style.display === "") {
-      element.style.display = "block";
-  } else {
-      element.style.display = "none";
-  }
+  element.classList.toggle('open');
 }
 
 // scroll header shrink
